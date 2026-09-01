f5095cfada6845b49d3ef0df070d0fd0e157a1dd
pedro-krefta <pedro_hk_duarte@estudante.sesisenai.org.br>
2026-09-01 09:54:42 -0300

    arrumando arquivos

--- backend/node.js
+++
@@ -1,9 +0,0 @@
-const {DataTypes } = require('sequelize')
-const db = require('./db/conn')
-
-const Usuario = db.define('usuario',{
-    codUSuario: {
-        type: DataTypes.INTEGER,
-        
-    }
-})
